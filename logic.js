24c69a9b643e0416a7fee73ccbe4f9bb30f53580
Christian Weider <christian.weider@pixlinemedia.de>
2021-01-27 06:57:55 +0100

    init

---
+++ logic.js
@@ -0,0 +1,117 @@
+$(document).ready(function(){
+    if(!localStorage.dsa_currentPage)
+    {
+        localStorage.dsa_currentPage = 'start';
+    }
+
+    var current = localStorage.dsa_currentPage;
+    if(!path[current])
+    {
+        localStorage.dsa_currentPage = 'fail';
+        current = 'fail';
+    }
+
+
+    window.obj = path[current];
+    if(!obj.success)
+    {
+        $('.submit-btn').hide();
+        $('.reset-btn').show();
+    }
+
+    $('.text').html(obj.text);
+
+    if(obj.distraction)
+    {
+        $('.submit-btn').hide();
+        $('.reset-btn').hide();
+
+    }
+
+    if(obj.checkSkill)
+    {
+        $('.checks').html('');
+        var skill = window.skills[obj.checkSkill];
+        skill.checks.forEach(function(stat, i){
+            var element = '<div class="stat"><div class="check-text">'+window.names[stat]+' ('+window.stats[stat]+'):</div><input min=1 max=20 type="number" id="skill_'+i+'"></div>';
+            $('.checks').append(element);
+        });
+        $('.checks').show();
+    }
+
+    if(obj.check)
+    {
+        var stat = obj.check;
+        $('.checks').html('');
+        var element = '<div class="stat"><div class="check-text">'+window.names[stat]+' ('+window.stats[stat]+'):</div><input min=1 max=20 type="number" id="stat"></div>';
+        $('.checks').append(element);
+
+        $('.checks').show();
+    }
+
+        $('.link-a-btn').on('click', function(){
+
+        localStorage.dsa_currentPage = obj.link_a;
+        window.location = "index.html";
+    });
+
+    $('.link-b-btn').on('click', function(){
+
+        localStorage.dsa_currentPage = obj.link_b;
+        window.location = "index.html";
+    });
+
+    $('.submit-btn').on('click', function(){
+
+        localStorage.dsa_currentPage = obj.success;
+
+        if(obj.checkSkill)
+        {
+            var skill = window.skills[obj.checkSkill];
+            var remaining = skill.value;
+            var ones = 0;
+            var twenties = 0;
+
+            skill.checks.forEach(function(c, i){
+                var stat = window.stats[c];
+                var rolled = parseInt($('input#skill_'+i).val());
+
+                if(rolled == 1)
+                {
+                    ones += 1;
+                }
+                if(rolled == 20)
+                {
+                    twenties += 1;
+                }
+                if(rolled > stat)
+                {
+                    remaining += (stat - rolled);
+                }
+            });
+
+            if((ones < 2) && (twenties > 1 || remaining < 0))
+            {
+                localStorage.dsa_currentPage = obj.fail;
+            }
+        }
+
+        if(obj.check)
+        {
+            var stat = window.stats[obj.check];
+            var rolled = parseInt($('input#stat').val());
+            if(rolled > stat)
+            {
+                localStorage.dsa_currentPage = obj.fail;
+            }
+        }
+
+        window.location = "index.html";
+    });
+
+    $('.reset-btn').on('click', function(){
+        localStorage.dsa_currentPage = 'start';
+        window.location = "index.html";
+    });
+});
+
---
+++ stats.js
@@ -0,0 +1,30 @@
+window.names = {
+    'MU': 'Mut',
+    'KL': 'Klugheit',
+    'IN': 'Intuition',
+    'CH': 'Charisma',
+    'FF': 'Fingerfertigkeit',
+    'GE': 'Gewandtheit',
+    'KO': 'Konstitution',
+    'KK': 'Körperkraft',
+    'AT': 'Attacke',
+    'PA': 'Parade'
+};
+
+window.stats = {
+    'MU': 12,
+    'KL': 12,
+    'IN': 12,
+    'CH': 12,
+    'FF': 12,
+    'GE': 12,
+    'KO': 12,
+    'KK': 12,
+    'AT': 15,
+    'PA': 14
+};
+
+window.skills = {
+    'Athletik':  {checks:['GE','KO','KK'], value: 6},
+    'Wildnis':  {checks:['IN','GE','KO'], value: 6},
+};
