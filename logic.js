e6d0b1cf24031d7931d372bba6d6d0e3131460ae
Christian Weider <christian.weider@pixlinemedia.de>
2021-02-19 16:21:23 +0100

    Wartungsmodus

--- logic.js
+++ logic.js
@@ -1,125 +1,144 @@
-$(document).ready(function(){
-    if(!localStorage.dsa_currentPage)
-    {
-        localStorage.dsa_currentPage = 'start';
+$(document).ready(function () {
+    window.setLS = function (key, value) {
+        var config = JSON.parse(localStorage.dsa);
+        config[key] = value;
+        localStorage.dsa = JSON.stringify(config);
+    };
+    window.getLS = function (key) {
+        var config = JSON.parse(localStorage.dsa);
+        if(!config[key])
+        {
+            return false;
+        }
+        return config[key];
+    };
+    window.setStat = function (key, value) {
+        var stats = getLS('stats');
+        if(!stats)
+        {
+            stats = {};
+        }
+        stats[key] = value;
+        setLS('stats',stats);
+    };
+    window.getStat = function (key) {
+        var stats = getLS('stats');
+        if(!stats || !stats[key])
+        {
+            return false;
+        }
+        return stats[key];
+    };
+
+    if (!localStorage.dsa) {
+        localStorage.dsa = '{}';
     }
 
-    var current = localStorage.dsa_currentPage;
-    if(!path[current])
-    {
-        localStorage.dsa_currentPage = 'fail';
+    if (!getLS('currentPage')) {
+        setLS('currentPage','start');
+    }
+    if (!!getLS('background')) {
+        $('.container').css('background-image', 'url("' + getLS('background') + '")');
+    }
+
+    var current = getLS('currentPage');
+    if (!path[current]) {
         current = 'fail';
+        setLS('currentPage',current);
     }
-
 
     window.obj = path[current];
-    if(!obj.success)
-    {
+    if (!obj.success) {
         $('.submit-btn').hide();
         $('.reset-btn').show();
     }
 
     $('.text').html(obj.text);
 
-    if(obj.distraction)
-    {
+    if (obj.distraction) {
         $('.submit-btn').hide();
         $('.reset-btn').hide();
 
     }
 
-    if(obj.checkSkill)
-    {
+    if (obj.checkSkill) {
         $('.checks').html('');
         var skill = window.skills[obj.checkSkill];
-        skill.checks.forEach(function(stat, i){
-            var element = '<div class="stat"><div class="check-text">'+window.names[stat]+' ('+window.stats[stat]+'):</div><input min=1 max=20 required type="number" id="skill_'+i+'"></div>';
+        skill.checks.forEach(function (stat, i) {
+            var element = '<div class="stat"><div class="check-text">' + window.names[stat] + ' (' + window.stats[stat] + '):</div><input min=1 max=20 required type="number" id="skill_' + i + '"></div>';
             $('.checks').append(element);
         });
         $('.checks').show();
     }
 
-    if(obj.check)
-    {
+    if (obj.check) {
         var stat = obj.check;
         $('.checks').html('');
-        var element = '<div class="stat"><div class="check-text">'+window.names[stat]+' ('+window.stats[stat]+'):</div><input min=1 max=20 required type="number" id="stat"></div>';
+        var element = '<div class="stat"><div class="check-text">' + window.names[stat] + ' (' + window.stats[stat] + '):</div><input min=1 max=20 required type="number" id="stat"></div>';
         $('.checks').append(element);
 
         $('.checks').show();
     }
 
-    $('.link-a-btn').on('click', function(){
-
-        localStorage.dsa_currentPage = obj.link_a;
-        window.location = "/";
+    $('.link-a-btn').on('click', function () {
+        setLS('currentPage', obj.link_a);
+        window.location.reload()
     });
 
-    $('.link-b-btn').on('click', function(){
-
-        localStorage.dsa_currentPage = obj.link_b;
-        window.location = "/";
+    $('.link-b-btn').on('click', function () {
+        setLS('currentPage', obj.link_b);
+        window.location.reload()
     });
 
-    $('.submit-btn').on('click', function(){
+    $('.submit-btn').on('click', function () {
+        setLS('currentPage', obj.success);
 
-        localStorage.dsa_currentPage = obj.success;
-
-        if(obj.checkSkill)
-        {
+        if (obj.checkSkill) {
             var skill = window.skills[obj.checkSkill];
             var remaining = skill.value;
             var ones = 0;
             var twenties = 0;
 
-            skill.checks.forEach(function(c, i){
+            skill.checks.forEach(function (c, i) {
                 var stat = window.stats[c];
-                var rolled = parseInt($('input#skill_'+i).val());
-                if(isNaN(rolled))
-                {
+                var rolled = parseInt($('input#skill_' + i).val());
+                if (isNaN(rolled)) {
                     rolled = 20;
                 }
 
-                if(rolled == 1)
-                {
+                if (rolled == 1) {
                     ones += 1;
                 }
-                if(rolled == 20)
-                {
+                if (rolled == 20) {
                     twenties += 1;
                 }
-                if(rolled > stat)
-                {
+                if (rolled > stat) {
                     remaining += (stat - rolled);
                 }
             });
 
-            if((ones < 2) && (twenties > 1 || remaining < 0))
-            {
-                localStorage.dsa_currentPage = obj.fail;
+            if ((ones < 2) && (twenties > 1 || remaining < 0)) {
+                setLS('currentPage', obj.fail);
             }
         }
 
-        if(obj.check)
-        {
+        if (obj.check) {
             var stat = window.stats[obj.check];
             var rolled = parseInt($('input#stat').val());
-            if(isNaN(rolled))
-            {
+            if (isNaN(rolled)) {
                 rolled = 20;
             }
-            if(rolled > stat)
-            {
-                localStorage.dsa_currentPage = obj.fail;
+            if (rolled > stat) {
+                setLS('currentPage', obj.fail);
             }
         }
 
-        window.location = "/";
+        window.location.reload()
     });
 
-    $('.reset-btn').on('click', function(){
-        localStorage.dsa_currentPage = 'start';
-        window.location = "/";
+    $('.reset-btn').on('click', function () {
+        setLS('currentPage', 'start');
+        window.location.reload()
     });
 });
 
